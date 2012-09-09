2cc1d406d72180f64de2323a99df1573a14c7005
Vjeux <vjeuxx@gmail.com>
2012-09-09 17:44:58 +0200

    Fix html issue

--- app/server/routes/main.js
+++ app/server/routes/main.js
@@ -5,17 +5,40 @@ module.exports = function(req, res, js){
   var composer = <div id="composer"></div>;
   var header = <div id="header"><h1><i class="icon-check"></i>Achieved</h1>{composer}</div>;
   var feed = <div id="feed"></div>;
-  var register_username = <div><input type="text"></input>;
+  var register_username = <input type="text"></input>;
   var register_password = <input type="password"></input>;
   var login_username = <input type="text"></input>;
 
   var login_password = <input type="password"></input>;
 
-  var footer_left = <ul><li>Register:</li><li>{register_username}</li><li>{register_password}</li></ul>;
-  var footer_right = <ul><li>Login:</li><li>{login_username}</li><li>{login_password}</li><li>{login_message}</li></ul>;
-  var footer = <div id="footer"><div class="left">{footer_left}</div><div class="right">{footer_right}</div></div>;
+  var footer_left = 
+    <ul>
+      <li>Register:</li>
+      <li>{register_username}</li>
+      <li>{register_password}</li>
+    </ul>;
 
-  var body = <div>{header}{feed} {footer}</div>;
+  var footer_right =
+    <ul>
+      <li>Login:</li>
+      <li>{login_username}</li>
+      <li>{login_password}</li>
+      <li>{login_message}</li>
+    </ul>;
+
+  var footer =
+    <div id="footer">
+      <div class="left">{footer_left}</div>
+      <div class="right">{footer_right}</div>
+    </div>;
+
+  var body =
+    <div>
+      {header}
+      {feed}
+      {footer}
+    </div>;
+
   js.dom(body);
 
   js.call('bind_username', register_username, register_password);
