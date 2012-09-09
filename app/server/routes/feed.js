e770186a4ef41b05d07f0be170fa7b1f01e6b9b3
Vjeux <vjeuxx@gmail.com>
2012-09-09 14:23:57 +0200

    Muhahah optimistic hack

--- app/server/routes/feed.js
+++ app/server/routes/feed.js
@@ -14,11 +14,12 @@ module.exports = function(req, res, js) {
     var n = achievements.length;
     achievements.forEach(function (achievement) {
       disruptDB.count_upvote(achievement._id, function(c) {
-        var upVote = <a class="action upVote">This is cool ({c})</a>;
+        var number = <span>{c}</span>;
+        var upVote = <a class="action upVote">This is cool ({number})</a>;
         var didIt = <a class="action didIt">I did it!</a>;
         var chicken = <a class="action chicken">Chicken someone!</a>;
         var goal = <a class="action goal">I{"'"}d love it!</a>;
-        js.call('upVote', upVote, achievement.achievement);
+        js.call('upVote', upVote, number, achievement.achievement);
         js.call('didIt', didIt, achievement.achievement);
         js.call('chicken', chicken, achievement.achievement);
         js.call('goal', goal, achievement.achievement);
